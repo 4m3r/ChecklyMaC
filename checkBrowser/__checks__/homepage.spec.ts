81d3d83adba4f2f4d62459445f7f223c5742440c
Amer Alic <amer@knowl.solutions>
2023-03-17 13:31:22 +0100

    Title

--- checkBrowser/__checks__/homepage.spec.ts
+++ checkBrowser/__checks__/homepage.spec.ts
@@ -4,6 +4,6 @@ import { test, expect } from '@playwright/test'
 test('Checkly Homepage', async ({ page }) => {
   const response = await page.goto('https://4m3r.github.io/pfpage/index.html')
   expect(response?.status()).toBeLessThan(400)
-  await expect(page).toHaveTitle(/KnowL/)
+  await expect(page).toHaveTitle(/My Portfolio/)
   await page.screenshot({ path: 'homepage.jpg' })
 })
