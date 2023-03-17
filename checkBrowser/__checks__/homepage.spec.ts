ba22e10fe8dd463e18615f97de3aada5af261add
Amer Alic <amer@knowl.solutions>
2023-03-17 13:28:14 +0100

    Update

--- checkBrowser/__checks__/homepage.spec.ts
+++ checkBrowser/__checks__/homepage.spec.ts
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test'
 
 test('Checkly Homepage', async ({ page }) => {
-  const response = await page.goto('https://dev.knowl.solutions')
+  const response = await page.goto('https://4m3r.github.io/pfpage/index.html')
   expect(response?.status()).toBeLessThan(400)
   await expect(page).toHaveTitle(/KnowL/)
   await page.screenshot({ path: 'homepage.jpg' })
